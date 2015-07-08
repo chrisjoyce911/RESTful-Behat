b8e8f343e34a7ef51b8895b5ce1f67622281265c
Chris Joyce <chrisj@benon.com>
2015-07-09 00:55:14 +1000

    Added more tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,104 +1,140 @@
 <?php
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
-use GuzzleHttp\Client;     
+	use Behat\Behat\Context\ClosuredContextInterface,
+		Behat\Behat\Context\TranslatedContextInterface,
+		Behat\Behat\Context\BehatContext,
+		Behat\Behat\Exception\PendingException;
+	use Behat\Gherkin\Node\PyStringNode,
+		Behat\Gherkin\Node\TableNode;
+	use GuzzleHttp\Client;
 
-class FeatureContext extends BehatContext
-{
-    public $_response;
-    public $_client;
-    private $_parameters = array();
-    /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param   array   $parameters     context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-    
-        $this->_parameters = $parameters;
-        $baseUrl = $this->getParameter('base_url');
-        $client = new Client(['base_url' => $baseUrl]);
-        $this->_client = $client;
-    }
+	class FeatureContext extends BehatContext
+	{
+		public $_response;
+		public $_client;
+		private $_parameters = array();
 
-    public function getParameter($name)
-    {
-        if (count($this->_parameters) === 0) {
-            throw new \Exception('Parameters not loaded!');
-        } else {
-            $parameters = $this->_parameters;
-            return (isset($parameters[$name])) ? $parameters[$name] : null;
-        }
-    }
+		/**
+		 * Initializes context.
+		 * Every scenario gets it's own context object.
+		 *
+		 * @param   array $parameters context parameters (set them up through behat.yml)
+		 */
+		public function __construct(array $parameters)
+		{
 
-    /**
-     * @When /^I request "([^"]*)"$/
-     */
-    public function iRequest($uri)
-    {
-        $request = $this->_client->get($uri);
-        $this->_response = $request;
-    }
+			$this->_parameters = $parameters;
+			$baseUrl           = $this->getParameter('base_url');
+			$client            = new Client(['base_url' => $baseUrl]);
+			$this->_client     = $client;
+		}
 
-    /**
-     * @Then /^the response should be JSON$/
-     */
-    public function theResponseShouldBeJson()
-    {
-        $data = json_decode($this->_response->getBody(true));
-	    echo $this->_response ;
-        if (empty($data)) { throw new Exception("Response was not JSON\n" . $this->_response);
-       }
-    }
+		public function getParameter($name)
+		{
+			if (count($this->_parameters) === 0) {
+				throw new \Exception('Parameters not loaded!');
+			} else {
+				$parameters = $this->_parameters;
+				return (isset($parameters[$name])) ? $parameters[$name] : null;
+			}
+		}
 
-    /**
-     * @Then /^the response status code should be (\d+)$/
-     */
-    public function theResponseStatusCodeShouldBe($httpStatus)
-    {
-        if ((string)$this->_response->getStatusCode() !== $httpStatus) {
-            throw new \Exception('HTTP code does not match '.$httpStatus.
-                ' (actual: '.$this->_response->getStatusCode().')');
-        }
-    }  
+		/**
+		 * @When /^I request "([^"]*)"$/
+		 */
+		public function iRequest($uri)
+		{
+			$request           = $this->_client->get($uri);
+			$this->_response   = $request;
+			$this->_requesturl = $this->getParameter('base_url') . $uri;
+		}
 
-    /**
-     * @Given /^the response has a "([^"]*)" property$/
-     */
-    public function theResponseHasAProperty($propertyName)
-    {
-        $data = json_decode($this->_response->getBody(true));
-        if (!empty($data)) {
-            if (!isset($data->$propertyName)) {
-                throw new Exception("Property '".$propertyName."' is not set!\n");
-            }
-        } else {
-            throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
-        }
-    }
-     /**
-     * @Then /^the "([^"]*)" property equals "([^"]*)"$/
-     */
-    public function thePropertyEquals($propertyName, $propertyValue)
-    {
-        $data = json_decode($this->_response->getBody(true));
+		/** @Then /^echo last response$/ */
+		public function echoLastResponse()
+		{
+			$this->printDebug(
+				$this->_requesturl . "\n\n" .
+				$this->_response
+			);
+		}
 
-        if (!empty($data)) {
-            if (!isset($data->$propertyName)) {
-                throw new Exception("Property '".$propertyName."' is not set!\n");
-            }
-            if ($data->$propertyName !== $propertyValue) {
-                throw new \Exception('Property value mismatch! (given: '.$propertyValue.', match: '.$data->$propertyName.')');
-            }
-        } else {
-            throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
-        }
-    }
-}
+		/**
+		 * @Given /^the response should contain json:$/
+		 */
+		public function theResponseShouldContainJson(PyStringNode $string)
+		{
+			$data = $this->_response->getBody(true);
+
+			$data   = json_decode($data, true);
+			$string = json_decode($string, true);
+			foreach ($data as $key => $val) {
+				if (isset($string[$key])) {
+					//echo $data[$key],' is ',$string[$key];
+					// echo PHP_EOL;
+				} else {
+					throw new Exception("Response did not match\n" . $key);
+				}
+			}
+		}
+
+		/**
+		 * @Then /^the response should be JSON$/
+		 */
+		public function theResponseShouldBeJson()
+		{
+			$data = json_decode($this->_response->getBody(true));
+			if (empty($data)) {
+				throw new Exception("Response was not JSON\n" . $this->_response);
+			}
+		}
+
+
+		/**
+		 * @Then /^the response status code should be (\d+)$/
+		 */
+		public function theResponseStatusCodeShouldBe($httpStatus)
+		{
+			if ((string)$this->_response->getStatusCode() !== $httpStatus) {
+				throw new \Exception(
+					'HTTP code does not match ' . $httpStatus .
+					' (actual: ' . $this->_response->getStatusCode() . ')'
+				);
+			}
+		}
+
+		/**
+		 * @Given /^the response has a "([^"]*)" property$/
+		 */
+		public function theResponseHasAProperty($propertyName)
+		{
+			$data = json_decode($this->_response->getBody(true));
+			if (!empty($data)) {
+				if (!isset($data->$propertyName)) {
+					throw new Exception("Property '" . $propertyName . "' is not set!\n");
+				}
+			} else {
+				throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
+			}
+		}
+
+		/**
+		 * @Then /^the "([^"]*)" property equals "([^"]*)"$/
+		 */
+		public function thePropertyEquals($propertyName, $propertyValue)
+		{
+			$data = json_decode($this->_response->getBody(true));
+
+			if (!empty($data)) {
+				if (!isset($data->$propertyName)) {
+					throw new Exception("Property '" . $propertyName . "' is not set!\n");
+				}
+				if ($data->$propertyName !== $propertyValue) {
+					throw new \Exception(
+						'Property value mismatch! (given: ' . $propertyValue . ', match: ' . $data->$propertyName . ')'
+					);
+				}
+			} else {
+				throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
+			}
+		}
+	}
--- features/github_rest_api.feature
+++ features/github_rest_api.feature
@@ -4,9 +4,58 @@ Feature: GitHub RestFul Api Testing with Behat
  I want to test restful api of the GitHub
  So that it will bring smile for behat community
 
+Scenario: GitHub behat Demo Api my repo
+	Given I request "/repos/chrisjoyce911/RESTful-Behat"
+	Then the response should be JSON
+	And the response status code should be 200
+	And the response has a "name" property
+	And the "name" property equals "RESTful-Behat"
+
 Scenario: GitHub behat Demo Api
-  Given I request "/repos/chrisjoyce911/BehatDemo"
-  Then the response should be JSON
-  And the response status code should be 200
-  And the response has a "name" property
-  And the "name" property equals "BehatDemo"
+	Given I request "/"
+	Then the response should be JSON
+  
+  Scenario: GitHub behat JAON equals
+	Given I request "/"
+	Then the response should be JSON
+	And the response should contain json:
+	"""
+{
+  "current_user_url": "https://api.github.com/user",
+  "current_user_authorizations_html_url": "https://github.com/settings/connections/applications{/client_id}",
+  "authorizations_url": "https://api.github.com/authorizations",
+  "code_search_url": "https://api.github.com/search/code?q={query}{&page,per_page,sort,order}",
+  "emails_url": "https://api.github.com/user/emails",
+  "emojis_url": "https://api.github.com/emojis",
+  "events_url": "https://api.github.com/events",
+  "feeds_url": "https://api.github.com/feeds",
+  "following_url": "https://api.github.com/user/following{/target}",
+  "gists_url": "https://api.github.com/gists{/gist_id}",
+  "hub_url": "https://api.github.com/hub",
+  "issue_search_url": "https://api.github.com/search/issues?q={query}{&page,per_page,sort,order}",
+  "issues_url": "https://api.github.com/issues",
+  "keys_url": "https://api.github.com/user/keys",
+  "notifications_url": "https://api.github.com/notifications",
+  "organization_repositories_url": "https://api.github.com/orgs/{org}/repos{?type,page,per_page,sort}",
+  "organization_url": "https://api.github.com/orgs/{org}",
+  "public_gists_url": "https://api.github.com/gists/public",
+  "rate_limit_url": "https://api.github.com/rate_limit",
+  "repository_url": "https://api.github.com/repos/{owner}/{repo}",
+  "repository_search_url": "https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}",
+  "current_user_repositories_url": "https://api.github.com/user/repos{?type,page,per_page,sort}",
+  "starred_url": "https://api.github.com/user/starred{/owner}{/repo}",
+  "starred_gists_url": "https://api.github.com/gists/starred",
+  "team_url": "https://api.github.com/teams",
+  "user_url": "https://api.github.com/users/{user}",
+  "user_organizations_url": "https://api.github.com/user/orgs",
+  "user_repositories_url": "https://api.github.com/users/{user}/repos{?type,page,per_page,sort}",
+  "user_search_url": "https://api.github.com/search/users?q={query}{&page,per_page,sort,order}"
+}
+    """
+	And the response should contain json:
+	"""
+{
+  "current_user_url": "https://api.github.com/user",
+  "user_search_url": "https://api.github.com/search/users?q={query}{&page,per_page,sort,order}"
+}
+    """
